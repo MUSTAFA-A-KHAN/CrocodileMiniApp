925ba76503ff43cb2b15f355ebb540905d7af81b
MUSTAFA-A-KHAN <mustafakhan62608@gmail.com>
2025-01-29 15:47:20 +0530

    feat: navigate to home on click of header

--- src/pages/Dashboard/components/Header.jsx
+++ src/pages/Dashboard/components/Header.jsx
@@ -1,9 +1,17 @@
 // components/Header.js
 import React from "react";
+import { useNavigate } from "react-router-dom"; // Import useNavigate
 
 const Header = () => {
+  const navigate = useNavigate(); // Initialize useNavigate
+
+  // Function to handle click and redirect to home
+  const handleClick = () => {
+    navigate("/"); // Redirect to the home route
+  };
+
   return (
-    <header className="header-title">
+    <header className="header-title" onClick={handleClick} style={{ cursor: "pointer" }}>
       <span className="header-highlight">Cr</span>ocodile Ga
       <span className="header-highlight">me</span> Engine
     </header>
